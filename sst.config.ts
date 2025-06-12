8bcd5657a9327bb77cdfcbe2a6c0f53f54435bd3
Aninda Deb <deb.aninda.aninda@gmail.com>
2025-06-12 19:58:30 +0530

    Securing the API

--- sst.config.ts
+++ sst.config.ts
@@ -5,12 +5,20 @@ export default $config({
     return {
       name: "monorepo-template",
       removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
+      // protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
     await import("./infra/storage");
     await import("./infra/api");
+    const auth = await import("./infra/auth");
+    return {
+      UserPool: auth.userPool.id,
+      Region: aws.getRegionOutput().name,
+      IdentityPool: auth.identityPool.id,
+      UserPoolClient: auth.userPoolClient.id,
+    };
   },
 });
+
